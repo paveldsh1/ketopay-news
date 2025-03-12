9d5997778cdf0d6de98108335def8482637ea7bf
paveldsh1 <pavel_dsh@mail.ru>
2025-03-13 01:11:38 +0400

    minor fix

--- jest.config.js
+++ jest.config.js
@@ -2,10 +2,10 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   transform: {
-    '^.+\\.tsx?$': 'ts-jest', // Обработка TypeScript
+    '^.+\\.tsx?$': 'ts-jest',
   },
   moduleNameMapper: {
-    '\\.(jpeg|jpg|png|gif|svg)$': '<rootDir>/__mocks__/fileMock.js',  // Мок для изображений
+    '\\.(jpeg|jpg|png|gif|svg)$': '<rootDir>/__mocks__/fileMock.js',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
   transformIgnorePatterns: ['node_modules/(?!your-module)'],
